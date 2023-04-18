b2c220ab34156b11a156db5b91d91e34954bdff2
Santi <bry13041960@gmail.com>
2023-04-18 15:27:15 +0200

    JavaScript

---
+++ escript/productos.js
@@ -0,0 +1,430 @@
+const productos = [
+    //Vestidos
+    {
+        id: "vestido-azul",
+        titulo: "Vestido Azul",
+        imagen: "./prod_img/vestido-azul.jpg",
+        categoria: {
+            nombre: "vestido",
+            id: "color"
+        },
+        precio: 20
+    },
+    {
+        id: "vestido-blanco",
+        titulo: "Vestido Blanco",
+        imagen: "./prod_img/vestido-blanco.jpg",
+        categoria: {
+            nombre: "vestido",
+            id: "color"
+        },
+        precio: 21
+    },
+    {
+        id: "vestido-naranja",
+        titulo: "Vestido Naranja",
+        imagen: "./prod_img/vestido-naranja.jpg",
+        categoria: {
+            nombre: "vestido",
+            id: "color"
+        },
+        precio: 18
+    },
+    {
+        id: "vestido-verde",
+        titulo: "Vestido Verde",
+        imagen: "./prod_img/vestido-verde.jpg",
+        categoria: {
+            nombre: "vestido",
+            id: "color"
+        },
+        precio: 20
+    },
+    {
+        id: "vestido-rojo",
+        titulo: "Vestido Rojo",
+        imagen: "./prod_img/vestido-rojo.jpg",
+        categoria: {
+            nombre: "vestido",
+            id: "color"
+        },
+        precio: 25
+    },
+    {
+        id: "vestido-rosa",
+        titulo: "Vestido Rosa",
+        imagen: "./prod_img/vestido-rosa.jpg",
+        categoria: {
+            nombre: "vestido",
+            id: "color"
+        },
+        precio: 28
+    },
+    {
+        id: "vestido-negro",
+        titulo: "Vestido Negro",
+        imagen: "./prod_img/vestido-negro.jpg",
+        categoria: {
+            nombre: "vestido",
+            id: "color"
+        },
+        precio: 20
+    },
+    {
+        id: "vestido-marron",
+        titulo: "Vestido Marrón",
+        imagen: "./prod_img/vestido-marron.jpg",
+        categoria: {
+            nombre: "vestido",
+            id: "color"
+        },
+        precio: 32
+    },
+    //Sudaderas
+    {
+        id: "sudadera-gris",
+        titulo: "Sudadera Moschino",
+        imagen: "./prod_img/sudadera-gris.jpg",
+        categoria: {
+            nombre: "sudadera",
+            id: "color"
+        },
+        precio: 89
+    },
+    {
+        id: "sudadera-verde",
+        titulo: "Sudadera Verde",
+        imagen: "./prod_img/sudadera-verde.jpg",
+        categoria: {
+            nombre: "sudadera",
+            id: "color"
+        },
+        precio: 30
+    },
+    {
+        id: "sudadera-marron",
+        titulo: "Sudadera Marrón",
+        imagen: "./prod_img/sudadera-marron.jpg",
+        categoria: {
+            nombre: "sudadera",
+            id: "color"
+        },
+        precio: 25
+    },
+    {
+        id: "sudadera-roja",
+        titulo: "Sudadera Roja",
+        imagen: "./prod_img/sudadera-roja.jpg",
+        categoria: {
+            nombre: "sudadera",
+            id: "color"
+        },
+        precio: 22
+    },
+    {
+        id: "sudadera-rosa",
+        titulo: "Sudadera Rosa",
+        imagen: "./prod_img/sudadera-rosa.jpg",
+        categoria: {
+            nombre: "sudadera",
+            id: "color"
+        },
+        precio: 18
+    },
+    {
+        id: "sudadera-amarilla",
+        titulo: "Sudadera Amarilla",
+        imagen: "./prod_img/sudadera-amarilla.jpg",
+        categoria: {
+            nombre: "sudadera",
+            id: "color"
+        },
+        precio: 20
+    },
+    //Abrigos
+    {
+        id: "abrigo-amarillo",
+        titulo: "Abrigo Amarillo",
+        imagen: "./prod_img/abrigo-amarillo.jpg",
+        categoria: {
+            nombre: "abrigo",
+            id: "color"
+        },
+        precio: 35
+    },
+    {
+        id: "abrigo-morado",
+        titulo: "Abrigo Morado",
+        imagen: "./prod_img/abrigo-morado.jpg",
+        categoria: {
+            nombre: "abrigo",
+            id: "color"
+        },
+        precio: 30
+    },
+    {
+        id: "abrigo-rojo",
+        titulo: "Abrigo Rojo",
+        imagen: "./prod_img/abrigo-rojo.jpg",
+        categoria: {
+            nombre: "abrigo",
+            id: "color"
+        },
+        precio: 40
+    },
+    {
+        id: "abrigo-verde",
+        titulo: "Abrigo Verde",
+        imagen: "./prod_img/abrigo-verde.jpg",
+        categoria: {
+            nombre: "abrigo",
+            id: "color"
+        },
+        precio: 32
+    },
+    //Chubasqueros
+    {
+        id: "chubasquero-amarillo",
+        titulo: "Chubasquero Amarillo",
+        imagen: "./prod_img/chubasquero-amarillo.jpg",
+        categoria: {
+            nombre: "chubasquero",
+            id: "color"
+        },
+        precio: 15
+    },
+    {
+        id: "chubasquero-azul",
+        titulo: "Chubasquero Azul",
+        imagen: "./prod_img/chubasquero-azul.jpg",
+        categoria: {
+            nombre: "chubasquero",
+            id: "color"
+        },
+        precio: 22
+    },
+    {
+        id: "chubasquero-gris",
+        titulo: "Chubasquero Gris",
+        imagen: "./prod_img/chubasquero-gris.jpg",
+        categoria: {
+            nombre: "chubasquero",
+            id: "color"
+        },
+        precio: 35
+    },
+    {
+        id: "chubasquero-verde",
+        titulo: "Chubasquero Verde",
+        imagen: "./prod_img/chubasquero-verde.jpg",
+        categoria: {
+            nombre: "chubasquero",
+            id: "color"
+        },
+        precio: 30
+    },
+    //Jersey
+    {
+        id: "jersey-blanco",
+        titulo: "Jersey Blanco",
+        imagen: "./prod_img/jersey-blanco.jpg",
+        categoria: {
+            nombre: "jersey",
+            id: "color"
+        },
+        precio: 20
+    },
+    {
+        id: "jersey-rojo",
+        titulo: "Jersey Rojo",
+        imagen: "./prod_img/jersey-rojo.jpg",
+        categoria: {
+            nombre: "jersey",
+            id: "color"
+        },
+        precio: 8
+    },
+    {
+        id: "jersey-marino",
+        titulo: "Jersey Marino",
+        imagen: "./prod_img/jersey-marino.jpg",
+        categoria: {
+            nombre: "jersey",
+            id: "color"
+        },
+        precio: 15
+    },
+    {
+        id: "jersey-marron",
+        titulo: "Jersey Marrón",
+        imagen: "./prod_img/jersey-marron.jpg",
+        categoria: {
+            nombre: "jersey",
+            id: "color"
+        },
+        precio: 12
+    },
+    {
+        id: "jersey-rosa",
+        titulo: "Jersey Rosa",
+        imagen: "./prod_img/jersey-rosa.jpg",
+        categoria: {
+            nombre: "jersey",
+            id: "color"
+        },
+        precio: 10
+    },
+    {
+        id: "jersey-azul",
+        titulo: "Jersey Azul",
+        imagen: "./prod_img/jersey-azul.jpg",
+        categoria: {
+            nombre: "jersey",
+            id: "color"
+        },
+        precio: 8
+    },
+    //Collares
+    {
+        id: "collar-rojo",
+        titulo: "Collar Rojo",
+        imagen: "./prod_img/collar-rojo.jpg",
+        categoria: {
+            nombre: "collar",
+            id: "color"
+        },
+        precio: 12
+    },
+    {
+        id: "collar-amarillo",
+        titulo: "Collar Amarillo",
+        imagen: "./prod_img/collar-amarillo.jpg",
+        categoria: {
+            nombre: "collar",
+            id: "color"
+        },
+        precio: 10
+    },
+    {
+        id: "collar-negro",
+        titulo: "Collar Negro",
+        imagen: "./prod_img/collar-negro.jpg",
+        categoria: {
+            nombre: "collar",
+            id: "color"
+        },
+        precio: 8
+    },
+    {
+        id: "collar-verde",
+        titulo: "Collar Verde",
+        imagen: "./prod_img/collar-verde.jpg",
+        categoria: {
+            nombre: "collar",
+            id: "color"
+        },
+        precio: 8
+    },
+    //Arneses
+    {
+        id: "arnes-rojo",
+        titulo: "Arnes Rojo",
+        imagen: "./prod_img/arnes-rojo.jpg",
+        categoria: {
+            nombre: "arnes",
+            id: "color"
+        },
+        precio: 15
+    },
+    {
+        id: "arnes-rosa",
+        titulo: "Arnes Rosa",
+        imagen: "./prod_img/arnes-rosa.jpg",
+        categoria: {
+            nombre: "arnes",
+            id: "color"
+        },
+        precio: 15
+    },
+    {
+        id: "arnes-azul",
+        titulo: "Arnes Azul",
+        imagen: "./prod_img/arnes-azul.jpg",
+        categoria: {
+            nombre: "arnes",
+            id: "color"
+        },
+        precio: 15
+    },
+    //Lazos
+    {
+        id: "lazo-rojo",
+        titulo: "Lazo rojo",
+        imagen: "./prod_img/lazo-rojo.jpg",
+        categoria: {
+            nombre: "lazo",
+            id: "color"
+        },
+        precio: 15
+    },
+    {
+        id: "lazo-amarillo",
+        titulo: "Lazo Amarillo",
+        imagen: "./prod_img/lazo-amarillo.jpg",
+        categoria: {
+            nombre: "lazo",
+            id: "color"
+        },
+        precio: 15
+    },
+    {
+        id: "lazo-rosa",
+        titulo: "Lazo Rosa",
+        imagen: "./prod_img/lazo-rosa.jpg",
+        categoria: {
+            nombre: "lazo",
+            id: "color"
+        },
+        precio: 15
+    },
+];
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+
+function cargarProductos() {
+
+    productos.forEach(item => {
+        
+        const div = document.createElement("div");
+        div.classList.add("item");
+        div.innerHTML = `
+            <figure>
+                <img src="${item.imagen}" alt="${item.titulo}">
+            </figure>
+            <div class="info-product">
+                <h2>${item.titulo}</h2>
+                <p class="price">${item.precio}€</p>
+                <button id="${item.id}">Añadir al carrito</button>
+            </div>    
+        `;
+
+        contenedorProductos.append(div);
+    })
+}
+
+cargarProductos();
+
+
+
+/*
+<div class="item">
+    <figure>
+        <img src="prod img/vestido-azul.jpg" alt="reloj">
+    </figure>
+    <div class="info-product">
+        <h2>Vestido Azul</h2>
+        <p class="price">20€</p>
+        <button>Añadir al carrito</button>
+    </div>
+</div>
+*/
